4977be02bcd4857bbcd647f7f47d6fba85b5d05a
LHLobato <luislobato35@gmail.com>
2025-06-03 13:59:42 -0300

    Reshape File Finished

---
+++ featureselection.py
@@ -0,0 +1,50 @@
+from sklearn.metrics import accuracy_score
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+import io
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MaxAbsScaler, MinMaxScaler 
+
+
+def loadData(file):
+    with open(file, 'r', encoding="utf8") as f:
+        data = f.readlines()
+    result = []
+    for d in data:
+        d = d.strip()
+        if (len(d) > 0):
+            result.append(d)
+    return result
+
+
+
+
+
+bad_requests = loadData('PreProcessedAnomalous.txt')
+good_requests = loadData('PreprocessedNormalTraining.txt')
+all_requests = bad_requests + good_requests
+
+labels_Bad = [1] * len(bad_requests)
+labels_Good = [0] * len(good_requests)
+labels = labels_Bad + labels_Good
+print ("Total requests : ",len(all_requests))
+print ("Bad requests: ",len(bad_requests))
+print ("Good requests: ",len(good_requests))
+res = 224
+vectorizer = TfidfVectorizer(analyzer="char", sublinear_tf=True,lowercase=False, max_features = res*res)
+X = vectorizer.fit_transform(all_requests)
+scaler = MaxAbsScaler()
+X = scaler.fit_transform(X)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, labels, test_size=0.2, random_state=42
+)
+classifier = RandomForestClassifier(n_estimators=200)
+classifier.fit(X_train, y_train)
+y_pred = classifier.predict(X_test)
+y_probs = classifier.predict_proba(X_test)[:, 1]  # Probabilidades da classe positiva
+test_auc = roc_auc_score(y_test, y_probs)
+print("AUC no conjunto de teste:", test_auc)
+print("Acurácia: ", accuracy_score(y_test, y_pred))
--- reshaping.py
+++ reshaping.py
@@ -7,7 +7,8 @@ import io
 from pyts.image import GramianAngularField, RecurrencePlot
 from transformers import BertModel, BertTokenizer
 import torch
-
+import os 
+import matplotlib.pyplot as plt
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import MinMaxScaler 
 def loadData(file):
@@ -20,7 +21,21 @@ def loadData(file):
             result.append(d)
     return result
 
-def extract_features(text):t
+def save_images(dataset_name, X_data, y_data, arg):
+    for i, (image, label) in enumerate(zip(X_data, y_data)):
+        class_name = "malicious" if label == 1 else "benign"
+        file_path = os.path.join(
+            base_dir, dataset_name, class_name, f"{class_name}_{i}.png"
+        )
+        if arg == "RPLOT":
+            colors = "binary"
+        else:
+            colors = "rainbow"
+        plt.imsave(file_path, image, cmap=colors)
+    print(f"Imagens salvas em {base_dir}/{dataset_name}")
+
+
+def extract_features(text, model, tokenizer):
     input_ids = torch.tensor([tokenizer.encode(text, add_special_tokens=True)])
     with torch.no_grad():
         outputs = model(input_ids)
@@ -45,11 +60,46 @@ print ("Total requests : ",len(all_requests))
 print ("Bad requests: ",len(bad_requests))
 print ("Good requests: ",len(good_requests))
 
-vectorizer = TfidfVectorizer(analyzer="char", sublinear_tf=True,lowercase=False, ngram_range=(3,3))
-X = vectorizer.fit_transform(all_requests).toarray()
-pca = PCA(n_components=1)
-X_1d = pca.fit_transform(X)
+all_requests = all_requests
+labels = labels
+res = 224
+pipeline = Pipeline([
+    ('tfidf', TfidfVectorizer(analyzer = 'char', sublinear_tf = True, lowercase=False, max_features= res*res, ngram_range=(3,3))),
+    ('pca', PCA(n_components=res)),
+    ('scaler', MinMaxScaler())
+])
 
+X_processed = pipeline.fit_transform(all_requests)
+
+image_reshapes = {
+    "GASF": GramianAngularField(method = "summation"),
+    "GADF": GramianAngularField(method = "difference"),
+    "RPLOT": RecurrencePlot(dimension=1,threshold='point', percentage=20) 
+}
+for image_type, transformer in image_reshapes.items():
+    X_1d_transformed = transformer.fit_transform(X_processed)
+    X_train, X_temp, y_train, y_temp = train_test_split(
+        X_1d_transformed, labels, test_size=0.3, stratify=labels, random_state=0
+    )
+    X_val, X_test, y_val, y_test = train_test_split(
+        X_temp, y_temp, test_size=0.5, stratify=y_temp, random_state=0
+    )
+    base_dir = f"datasets/TFIDV/{str(image_type)}"
+    train_dir, val_dir, test_dir = [
+        os.path.join(base_dir, d) for d in ["train", "val", "test"]
+    ]
+    for subdir in [train_dir, val_dir, test_dir]:
+        os.makedirs(os.path.join(subdir, "benign"), exist_ok=True)
+        os.makedirs(os.path.join(subdir, "malicious"), exist_ok=True)
+    
+    datasets = {
+        "train": (X_train, y_train),
+        "val": (X_val, y_val),
+        "test": (X_test, y_test),
+    }
+    for dataset_name, (X_data, y_data) in datasets.items():
+        save_images(dataset_name, X_data, y_data, image_type)
+    print(f"Imagens {image_type}, geradas e salvas com sucesso!")
 
 #model = BertModel.from_pretrained('bert-base-uncased', output_hidden_states=True)
 #tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
